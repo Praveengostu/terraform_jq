8ea6012f3be0276ed91a353624bb125db0a6fd92
Praveengostu <praveengostu@gmail.com>
2020-01-07 11:05:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "eval \"$(jq -r '@sh \"FOO=\(.foo) BAZ=\(.baz)\"')\";FOOBAZ=\"$FOO $BAZ\";jq -n --arg foobaz \"$FOOBAZ\" '{\"foobaz\":$foobaz}'"
+    command = "jq"
   }
 }
