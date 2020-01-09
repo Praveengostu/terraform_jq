1df0dee7f03ca7b8a53ccf339e916fbfdc6104eb
Praveengostu <praveengostu@gmail.com>
2020-01-09 11:25:06 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,5 @@
-echo '{"foo": 0}' | jq .
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -P /tmp/
+chmod 777 /tmp/jq-linux64
+cmd=`cat tmp.json | /tmp/jq-linux64 -c .[] `
+eval "$cmd"
+echo '{"foo": 0}' | /tmp/jq-linux64 .
